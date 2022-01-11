19f2059d467cb64a794b7b7119b96bce8bfc04eb
TrevourGuild <trevorr.guildd@gmail.com>
2022-01-11 12:52:39 -0700

    got the form rendered

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
+const initialValue = {
+  fullName: "",
+  email: "",
+  password: "",
+  occupation: [],
+  state: []
+}
+
+
+
 function App() {
+
+  const [items, setItems] = React.useState([])
+
+  React.useEffect(() =>{
+    async function getDropdownValues(){
+      const response = await fetch('https://frontend-take-home.fetchrewards.com/form')
+      const body = await response.json()
+      setItems(body.results.map(({})))
+    }
+  })
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='create-user-form'>
+    <form>
+      <h1>Create User Form</h1>
+      <label>
+        Full Name:
+        <input
+            type = 'text'
+            name= "fullName"
+        />
+      </label>
+      <label>
+        Email:
+        <input
+            type = 'text'
+            name="email"
+        />
+      </label>
+      <label>
+        Password:
+        <input
+            type = 'text'
+            name="password"
+        />
+      </label>
+
+      <form action = "https://frontend-take-home.fetchrewards.com/form" method = 'GET'>
+      <label> Occupation:
+      <select
+      >
+      <option value = {"occupations"}>- Select an Occupation -</option>
+      </select>
+      </label>
+
+
+      <label> State:
+      <select>
+      <option value = "states">- Select a state -</option>
+      </select>
+      </label>
+      </form>
+      
+
+      <input type = 'submit' value = "Submit"/>
+    </form>
+
+
+
     </div>
   );
 }
